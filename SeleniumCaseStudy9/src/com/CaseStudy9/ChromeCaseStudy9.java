1ba29c32f248f0775376a0ff9779afa3159eeaf7
Bishakha1002 <bishakhabharti1002@gmail.com>
2026-06-15 23:25:03 +0530

    Update ChromeCaseStudy9.java

--- SeleniumCaseStudy9/src/com/CaseStudy9/ChromeCaseStudy9.java
+++ SeleniumCaseStudy9/src/com/CaseStudy9/ChromeCaseStudy9.java
@@ -74,14 +74,14 @@ public class ChromeCaseStudy9 {
 
         Object[][] data = new Object[3][2];
 
-        data[0][0] = "raghubn@gmail.com";
-        data[0][1] = "raghubn";
+        data[0][0] = "nehaBharti@gmail.com";
+        data[0][1] = "neh@";
 
-        data[1][0] = "9880614548";
-        data[1][1] = "raghubn@123";
+        data[1][0] = "bhartijha@gmail.com";
+        data[1][1] = "bh@rti";
 
-        data[2][0] = "Wiprotechnologies";
-        data[2][1] = "1234567890";
+        data[2][0] = "bishakha@gmail.com";
+        data[2][1] = "bish@kha";
 
         return data;
     }
